fa8bfee1398e653e343cfaf1f1d051b2975d4ebe
Filip Garcia <mailafilip@gmail.com>
2021-06-22 14:46:34 +0200

    Updated positions and Main and added classes

---
+++ src/main/java/Arena.java
@@ -0,0 +1,9 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Arena {
+
+    public final char wall = '\u2588';
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Lets do this");
+
+        TerminalSize ts = new TerminalSize(70, 30);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        terminalFactory.setInitialTerminalSize(ts);
+        Terminal terminal = terminalFactory.createTerminal();
     }
 }
