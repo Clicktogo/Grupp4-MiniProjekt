e194c075d0d4d785243db8156fb4684150cf1847
Pauline <pauline.j.andersson@gmail.com>
2021-06-22 16:10:50 +0200

    added snake start position and body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
@@ -15,6 +17,16 @@ public class Main {
         Terminal terminal = terminalFactory.createTerminal();
 
         System.out.println(terminal.getTerminalSize());
+
+        Snake snake = new Snake(4);
+        Position start = snake.getStartPosition();
+        terminal.setCursorPosition(start.getX(),start.getY());
+        terminal.putCharacter(snake.snakeChar);
+
+        for(int i = 0; i<snake.getLength(); i++){
+            terminal.setCursorPosition(start.getX()+i,start.getY());
+            terminal.putCharacter(snake.snakeChar);
+        }
 
         Arena arena = new Arena();
         arena.generateHorizontalWall(terminal, ts);
--- src/main/java/Snake.java
+++ src/main/java/Snake.java
@@ -1,2 +1,40 @@
+import java.util.List;
+import java.util.Random;
+
 public class Snake {
+    final char snakeChar = 'o';
+    private int length;
+    private List<Position> position;
+
+    public Snake(int length) {
+        this.length = length;
+    }
+
+    public List<Position> getPosition() {
+        return position;
+    }
+
+    public void setPosition(List<Position> position) {
+        this.position = position;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Position getStartPosition() {
+        Random r = new Random();
+        int low1 = 20;
+        int high1 = 50;
+        int low2 = 10;
+        int high2 = 20;
+
+        int randomHorizontal = r.nextInt(high1-low1) + low1;
+        int randomVertical = r.nextInt(high2-low2) + low2;
+        return new Position(randomHorizontal, randomVertical);
+    }
 }
